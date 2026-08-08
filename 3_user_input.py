f11cdcd5e4af20d8320f27d683663db73214ebcd
“[Enes <[eneswichai@gmail.com]>
2026-08-08 14:02:32 +0100

    loops exercise

--- 3_user_input.py
+++ 3_user_input.py
@@ -1,1 +1,8 @@
 
+apple = input("would u like an apple?")
+second = float(input("how much does it cost"))
+third = int(input("how many would u like?"))
+
+total = second * third
+
+print(f"so yo want apple and the cost is {second} and u want {third} apples. your total will be {round(total, 2)}")
